89c0536ae7b82bb67f8b79f4b233b14c836e8084
manthan-l-simformsolutions <manthan.l@simformsolutions.com>
2021-03-19 10:40:11 +0530

    table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TablePagination, TableRow } from '@material-ui/core';
 
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+  tableRow: {
+    backgroundColor: "black"
+  },
+  tableCell: {
+    color: "white",
+    fontSize: 20,
+    fontFamily: "Akaya Kanadaka",
+  }
+});
 function App() {
+  const [data, setData] = useState([]);
+  const classes = useStyles()
+  useEffect(async () => {
+    const result = await axios.get("https://jsonplaceholder.typicode.com/users")
+    setData(result.data)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow className={classes.tableRow}>
+            <TableCell className={classes.tableCell}>Name</TableCell>
+            <TableCell className={classes.tableCell} align="right">E-mail</TableCell>
+            <TableCell className={classes.tableCell} align="right">City</TableCell>
+            <TableCell className={classes.tableCell} align="right">Phone</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.email}</TableCell>
+              <TableCell align="right">{row.address.city}</TableCell>
+              <TableCell align="right">{row.phone}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
